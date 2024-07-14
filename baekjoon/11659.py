f24b99ae1bd9aed1e34bf725980ecb5cafd7dd11
Ju Ran KANG <97214987+Jurannn@users.noreply.github.com>
2024-07-14 10:49:34 +0900

    Create 11659.py

---
+++ baekjoon/11659.py
@@ -0,0 +1,39 @@
+```
+[합배열]
+S[0] = A[0] # 초기화
+S[i] = S[i-1] + A[i]
+
+구간합 ; S[j]-S[i-1] ; i에서 j까지 구간 합
+```
+
+
+
+#입력값 받기
+N, M = map(int, input().split())
+num_arr = list(map(int, input().split()))
+
+idx_arr = []
+for i in range(M):
+  i, j = map(int, input().split())
+  idx_arr.append((i-1,j-1))
+
+
+#부분합 array 생성
+sum_arr = [0 for i in range(N)] #공간 생성!!!!
+for i in range(N):
+  if i == 0:
+    sum_arr[0] = num_arr[0]
+  else:
+    sum_arr[i] = sum_arr[i-1]+num_arr[i]
+
+
+#i번째 수부터 j번째 수까지 합 출력
+for idx in range(M):
+  i = idx_arr[idx][0]
+  j = idx_arr[idx][1]
+  if i == j :
+    print(num_arr[i])
+  elif i == 0:
+    print(sum_arr[j])
+  else:
+    print(sum_arr[j] - sum_arr[i-1])
